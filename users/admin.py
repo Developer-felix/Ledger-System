69c93d61cdee1990609679a59995db0fb09dcd06
Developer-felix <onjombafelix1@gmail.com>
2022-05-02 17:31:07 -0700

    Cretaed some logic

--- users/admin.py
+++ users/admin.py
@@ -7,5 +7,7 @@ Working Place : Freelancer
 Email : onjombafelix1@gmail.com
 """
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+admin.site.register(User)
--- wallet/views.py
+++ wallet/views.py
@@ -26,4 +26,105 @@ from .logic import *
 #create post request for Create X amount to users one of the account.
 @api_view(['POST'])
 def create_x_amount_to_users(request):
-    pass   
+    if request.method == "POST":
+        data = request.data
+        print(data)
+        serializer = WalletSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status': 'success',
+                'error': False,
+                'error_message': '',
+                'message': 'User created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'failed',
+            'error': True,
+            'error_message': '',
+            'message': 'User not created',
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+#Debit X amount from users one of the account.
+@api_view(['POST'])
+def debit_x_amount_from_users(request):
+    if request.method == "POST":
+        data = request.data
+        print(data)
+        serializer = WalletSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status': 'success',
+                'error': False,
+                'error_message': '',
+                'message': 'User created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'failed',
+            'error': True,
+            'error_message': '',
+            'message': 'User not created',
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+#Transfer money from one account to another account for a single user.
+@api_view(['POST'])
+def transfer_money_from_one_account_to_another_account(request):
+    if request.method == "POST":
+        data = request.data
+        print(data)
+        serializer = WalletSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status': 'success',
+                'error': False,
+                'error_message': '',
+                'message': 'User created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'failed',
+            'error': True,
+            'error_message': '',
+            'message': 'User not created',
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)  
+
+
+
+#Transfer money from one account of user to another account of another user.
+@api_view(['POST'])
+def transfer_money_from_one_account_to_another_account_of_another_user(request):
+    if request.method == "POST":
+        data = request.data
+        print(data)
+        serializer = WalletSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'status': 'success',
+                'error': False,
+                'error_message': '',
+                'message': 'User created successfully',
+                'data': serializer.data
+            }, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'failed',
+            'error': True,
+            'error_message': '',
+            'message': 'User not created',
+            'data': serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
